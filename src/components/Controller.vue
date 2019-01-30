f9e6c50395a3f4d92d80b6140a0f5dab7fe8e6ff
David Nowinsky <david@nowinsky.net>
2019-01-30 15:00:03 +0100

    Simpliest controller ever, with input ranges

---
+++ src/components/Controller.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="controller">
+    <div>
+      Alpha
+      <input type="range" min=0 max=1 step=0.01 v-model="alpha">
+      {{ alpha }}
+    </div>
+    <div>
+      Beta1
+      <input type="range" min=0 max=1 step=0.01 v-model="beta1">
+      {{ beta1 }}
+    </div>
+    <div>
+      Beta2
+      <input type="range" min=0 max=1 step=0.01 v-model="beta2">
+      {{ beta2 }}
+    </div>
+    <div>
+      Beta3
+      <input type="range" min=0 max=1 step=0.01 v-model="beta3">
+      {{ beta3 }}
+    </div>
+    <div>
+      Gamma
+      <input type="range" min=0 max=1 step=0.01 v-model="gamma">
+      {{ gamma }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Controller',
+  props: {
+  },
+  data () {
+    return {
+      alpha: 1,
+      beta1: 0,
+      beta2: 0,
+      beta3: 0,
+      gamma: 0
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.controller {
+  text-align: left;
+}
+</style>
---
+++ tests/unit/controller.spec.js
@@ -0,0 +1,21 @@
+import { should } from 'chai'
+should()
+import { shallowMount } from '@vue/test-utils'
+import Controller from '@/components/Controller.vue'
+
+describe('Controller.vue', () => {
+  it('should display five inputs for ranges from 0 to 1', () => {
+    const wrapper = shallowMount(Controller, {
+      propsData: {}
+    })
+
+    const inputsWrapperArray = wrapper.findAll('input')
+    inputsWrapperArray.should.have.lengthOf(5)
+    inputsWrapperArray.wrappers.forEach(inputWrapper => {
+      inputWrapper.attributes('type').should.eql('range')
+      inputWrapper.attributes('min').should.eql('0')
+      inputWrapper.attributes('max').should.eql('1')
+      inputWrapper.attributes('step').should.eql('0.01')
+    })
+  })
+})
